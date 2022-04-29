ada516524a02d7ed215378cf517f2e02352e39cf
anhlh-3049 <le.hoang.anh@sun-asterisk.com>
2022-04-29 09:08:09 +0700

    Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.user.email_regex
 
+  attr_accessor :remember_token
+
   validates :name, presence: true
   validates :email, presence: true,
             length: {maximum: Settings.user.email.max_length},
@@ -17,6 +19,35 @@ class User < ApplicationRecord
                BCrypt::Engine.cost
              end
       BCrypt::Password.create string, cost: cost
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  class << self
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 
