8407148b086cda03d6f4b65e07508ea528c3c00d
anhlh-3049 <le.hoang.anh@sun-asterisk.com>
2022-04-29 09:08:09 +0700

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  get "/home" ,to:"static_pages#home"
+  get "/help" ,to:"static_pages#help"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
-  resources :users, only: %i(new create)
+  root "static_pages#home"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
 end
